afd976bf10992197416f06071e1d843a14d00285
TakuoIshida <archersparadox.100@gmail.com>
2020-10-10 13:45:04 +0900

    api settings

---
+++ bitflyer/bitflyer.go
@@ -0,0 +1,74 @@
+package bitflyer
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+const baseURL = "https://api.bitflyer.com/v1/"
+
+type APIClient struct {
+	key        string
+	secret     string
+	httpClient *http.Client
+}
+
+func (api APIClient) header(method, endpoint string, body []byte) map[string]string {
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+	fmt.Println(timestamp)
+	message := timestamp + method + endpoint + string(body)
+	mac := hmac.New(sha256.New, []byte(api.secret))
+	mac.Write([]byte(message))
+	sign := hex.EncodeToString(mac.Sum(nil))
+	return map[string]string{
+		"ACCESS-KEY":       api.key,
+		"ACCESS-TIMESTAMP": timestamp,
+		"ACCESS-SIGN":      sign,
+		"Content-Type":     "application/json",
+	}
+}
+
+func (api *APIClient) doRequest(method, urlPath string, query map[string]string, data []byte) (body []byte, er error) {
+	baseURL, err := url.Parse(baseURL)
+	if err != nil {
+		return
+	}
+	apiURL, err := url.Parse(urlPath)
+	if err != nil {
+		return
+	}
+	endpoint := baseURL.ResolveReference(apiURL).String()
+	fmt.Println(endpoint)
+
+	req, err := http.NewRequest(method, endpoint, bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+	q := req.URL.Query()
+	for key, value := range query {
+		q.Add(key, value)
+	}
+	req.URL.RawQuery = q.Encode()
+	// header情報にAPI認証情報を追加してあげる
+	for key, value := range api.header(method, req.URL.RequestURI(), data) {
+		req.Header.Add(key, value)
+	}
+	resp, err := api.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
